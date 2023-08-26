42d5ac06224193f2b51c31d0fce04c1c4ed6688f
Barlogdao <109853370+Barlogdao@users.noreply.github.com>
2023-08-26 18:38:24 +0300

    Add Enviorment
    Добавлено здание, стены и пол 1 уровня

---
+++ Assets/Graphics/Level1Visuals/LogoBehaviour.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+using DG.Tweening;
+
+namespace DTJ.Visual
+{
+    public class LogoBehaviour : MonoBehaviour
+    {
+        private void Start()
+        {
+            transform.DOScale(1.3f,1f).SetEase(Ease.OutSine).SetLoops(-1,LoopType.Yoyo);
+        }
+    }
+}
